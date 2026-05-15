e68074c8bdd8eb64df05de4149a64a580ebe681c
Duygu Ersoy <duyguersoy606@gmail.com>
2026-05-16 01:42:43 +0300

    Revert "change copial"
    This reverts commit 4debdc000e156c3f7482c4efcd65152b2f7b24b4.

--- src/executors/DualThresholding.py
+++ src/executors/DualThresholding.py
@@ -19,6 +19,7 @@ class DualThresholding(Component):
 
         self.request.model = PackageModel(**self.request.data)
 
+        self.type = self.request.get_param("configDualType")
 
         self.image_a = self.request.get_param("inputImageA")
         self.image_b = self.request.get_param("inputImageB")
