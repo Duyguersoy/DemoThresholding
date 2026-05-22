b6c9f20dfaa8e3873d760f82678e0fed3e6c38d8
Duygu Ersoy <duyguersoy606@gmail.com>
2026-05-22 03:57:26 +0300

    Fix thresholding executor config compatibility

--- src/models/PackageModel.py
+++ src/models/PackageModel.py
@@ -394,25 +394,7 @@ class DualThresholdingExecutor(Config):
 
 class ConfigExecutor(Config):
     name: Literal["ConfigExecutor"] = "ConfigExecutor"
-
-    value: Union[ThresholdingExecutor, DualThresholdingExecutor] = ThresholdingExecutor(
-        value=ThresholdingRequest(
-            inputs=None,
-            configs=ThresholdingConfigs(
-                configType=ConfigType(
-                    value=ConfigTypeGlobalThresholding(
-                        configEdit=ConfigGlobalType(
-                            value=ConfigTypeBlackWhite(
-                                thresholdVal=ConfigThresholdVal(),
-                                maxVal=ConfigMaxVal()
-                            )
-                        )
-                    )
-                )
-            )
-        )
-    )
-
+    value: Union[ThresholdingExecutor, DualThresholdingExecutor]
     type: Literal["executor"] = "executor"
     field: Literal["dependentDropdownlist"] = "dependentDropdownlist"
 
